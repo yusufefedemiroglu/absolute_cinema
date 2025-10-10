13b844237b06c066725900ef3f215f04710baf1d
shadow <shadow@Mac.home>
2025-10-10 13:58:09 +0300

    updated gitignore

--- absolute_backend/obj/Debug/net8.0/absolute_backend.AssemblyInfo.cs
+++ absolute_backend/obj/Debug/net8.0/absolute_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("absolute_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db5db0a3e7c356dcf717e731edd376f10bf3a56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0aaba493bb0146aa0f5a43b8d30ba29c64b0885")]
 [assembly: System.Reflection.AssemblyProductAttribute("absolute_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("absolute_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
