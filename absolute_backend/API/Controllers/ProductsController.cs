cf971779e8e65bc344798e8f6aea5eae74d10077
shadow <shadow@shadow-MacBook-Pro.local>
2025-11-18 12:04:14 +0300

    feat(backend): renewed products controller

--- absolute_backend/API/Controllers/ProductsController.cs
+++ absolute_backend/API/Controllers/ProductsController.cs
@@ -1,6 +1,5 @@
 using Application.DTOs.Product;
 using Application.Services;
-using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -16,6 +15,7 @@ public class ProductsController : ControllerBase
         _productService = productService;
     }
 
+    // get all products
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
@@ -23,68 +23,51 @@ public class ProductsController : ControllerBase
         return Ok(products);
     }
 
+    //get by id guid.
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById(Guid id)
     {
         var product = await _productService.GetByIdAsync(id);
         if (product == null)
-            return NotFound();
+            return NotFound(new { Message = "Product not found." });
 
         return Ok(product);
     }
 
+    // getbytitleid
     [HttpGet("by-title/{titleId:int}")]
     public async Task<IActionResult> GetByTitleId(int titleId)
     {
         var products = await _productService.GetByTitleIdAsync(titleId);
         return Ok(products);
     }
-
-    [HttpPost]
-    public async Task<IActionResult> Create([FromBody] ProductCreateDto product)
+    // create with dto
+    [HttpPost("{titleId:int}")]
+    public async Task<IActionResult> Create(int titleId, [FromBody] ProductCreateDto dto)
     {
-        ModelState.Remove("Title");
-        if (!ModelState.IsValid)
-            return BadRequest(ModelState);
-
-
-        // Map DTO to Product
-        var id = await _productService.CreateAsync(new Product
-        {
-            TitleId = product.TitleId,
-            Name = product.Name,
-            Description = product.Description,
-            Price = product.Price,
-            ImageUrl = product.ImageUrl,
-            Stock = product.Stock
-        });
-        return CreatedAtAction(nameof(GetById), new { id }, product);
+        var id = await _productService.CreateAsync(titleId, dto);
+        return CreatedAtAction(nameof(GetById), new { id }, dto);
     }
 
+    // update with dto
     [HttpPut("{id:guid}")]
-    public async Task<IActionResult> Update(Guid id, [FromBody] ProductUpdateDto product)
+    public async Task<IActionResult> Update(Guid id, [FromBody] ProductUpdateDto dto)
     {
-        var updated = await _productService.UpdateAsync(id, new Product
-        {
-            TitleId = product.TitleId ?? 0,
-            Name = product.Name ?? string.Empty,
-            Description = product.Description ?? string.Empty,
-            Price = product.Price ?? 0,
-            ImageUrl = product.ImageUrl ?? string.Empty,
-            Stock = product.Stock ?? 0
-        });
+        var updated = await _productService.UpdateAsync(id, dto);
+
         if (!updated)
-            return NotFound();
+            return NotFound(new { Message = "Product not found." });
 
         return NoContent();
     }
 
+    // delete by id
     [HttpDelete("{id:guid}")]
     public async Task<IActionResult> Delete(Guid id)
     {
         var deleted = await _productService.DeleteAsync(id);
         if (!deleted)
-            return NotFound();
+            return NotFound(new { Message = "Product not found." });
 
         return NoContent();
     }
