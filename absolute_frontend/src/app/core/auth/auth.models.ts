54c18c8444aece9d33a4ea7cbafcbc5c93f0e74c
shadow <shadow@Mac.home>
2026-01-14 14:28:01 +0300

    fix(frontend): we dont need register anymore

--- absolute_frontend/src/app/core/auth/auth.models.ts
+++ absolute_frontend/src/app/core/auth/auth.models.ts
@@ -2,12 +2,12 @@ export type LoginRequestDto = {
     userNameOrEmail: string;
     password: string;
 };
-export type RegisterRequestDto = {
-  userName: string;
-  email: string;
-  fullName: string;
-  password: string;
-};
+// export type RegisterRequestDto = {
+//   userName: string;
+//   email: string;
+//   fullName: string;
+//   password: string;
+// };
 
 export type AuthResponseDto = {
     accessToken: string;
--- absolute_frontend/src/app/core/auth/auth.service.ts
+++ absolute_frontend/src/app/core/auth/auth.service.ts
@@ -1,17 +1,11 @@
-import { inject, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import { AuthResponseDto,RegisterRequestDto,LoginRequestDto } from "./auth.models";
+import { AuthResponseDto,LoginRequestDto } from "./auth.models";
 @Injectable({providedIn: 'root'})
 export class AuthService {
     private readonly baseUrl = `${environment.apiUrl}/auth`;
     constructor(private http: HttpClient) {}
-
-    register(dto: RegisterRequestDto) {
-        return this.http.post<AuthResponseDto>(`${this.baseUrl}/register`, dto,{
-            withCredentials: true
-        });
-    }
 
     login(dto: LoginRequestDto) {
         return this.http.post<AuthResponseDto>(`${this.baseUrl}/login`, dto,{
