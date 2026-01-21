efc5f455f33c07f18e19dbdeac92d8d0355f4b2c
shadow <shadow@Mac.home>
2026-01-21 03:19:33 +0300

    fix(frontend): fixed config

--- absolute_frontend/src/app/app.config.ts
+++ absolute_frontend/src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi,withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
